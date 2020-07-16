3a4736a4c0a5cee95a3c223a610ecbdee839a337
geertza <geertza@gmail.com>
2020-07-15 18:26:38 -0600

    sort works needs asc/desc choice

--- src/components/personList.js
+++ src/components/personList.js
@@ -4,7 +4,9 @@ import Axios from 'axios';
 export default class PersonList extends React.Component {
     state = {
         persons:[],
-        search:''
+        search:'',
+        filteredPersons:'',
+        sort:'age',
     }
 
 
@@ -18,20 +20,52 @@ componentDidMount() {
 updateSearch(event){
     this.setState({search:event.target.value.substr(0,10)})
 }
+sortChange(event){
+    this.setState({sort:event.target.value})
+    }
+
+     
 render(){
     let filteredPersons = this.state.persons.filter(
         (persons) =>{
             return persons.name.first.toLowerCase().indexOf(this.state.search.toLowerCase()) !==-1 ;
         }
     )
+    let list =''
+    switch(this.state.sort) {
+        case 'age':
+          list =filteredPersons.sort((a,b)=> a.dob.age > b.dob.age ? 1:-1)
+          break;
+        case 'name':
+         list =filteredPersons.sort((a,b)=> a.name.first > b.name.first ? 1:-1)
+          break;
+        default:
+            list = filteredPersons
+      }
+   
+    
     return (
         <div className="empBody">
-        <input type="text" 
-        value={this.state.search}
-        onChange={this.updateSearch.bind(this)}
-        />
-        
-        {filteredPersons.map(person => (
+            <div className='searchBar'>
+            <label>First Name Search</label>
+            <input type="text"
+            id='nameSearch' 
+            value={this.state.search}
+            onChange={this.updateSearch.bind(this)}
+            />
+            <form onSubmit={this.handleSubmit}>
+        <label>
+          Sort By
+          <select value={this.state.value} onChange={this.sortChange.bind(this)}>
+            <option value="">default</option>
+            <option value="age">Age</option>
+            <option value="name">First Name</option>
+          </select>
+        </label>
+      </form>
+            </div>
+        <div className="grid">
+        {list.map(person => (
             <div className="empBlock">
             <ul>
                 <li><img src={person.picture.medium} alt=''></img></li>   
@@ -41,8 +75,9 @@ render(){
                 <li key={person.email}>{person.email}</li>
                 <li key={person.phone}>{person.location.city}{person.location.state}</li>
             </ul>
-            </div>
-        ))}
+                </div>
+            ))}
+        </div>
     </div>
     )
 }
